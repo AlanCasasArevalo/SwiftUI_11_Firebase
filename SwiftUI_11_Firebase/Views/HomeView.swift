ec3581d72075e830c6e92810f5fc4a13a04bcd01
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-01 10:52:25 +0200

    [FEATURE][HOME] * New post button was added * New Screen was added to add new posts

--- SwiftUI_11_Firebase/Views/HomeView.swift
+++ SwiftUI_11_Firebase/Views/HomeView.swift
@@ -12,6 +12,7 @@ import FirebaseAuth
 struct HomeView: View {
     
     @Binding var isLogged: Bool
+    @State var showModal: Bool = false
     
     var body: some View {
         NavigationView{
@@ -21,12 +22,33 @@ struct HomeView: View {
                         Text("Post \(item)")
                     }
                 }
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "plus")
+                                .font(.system(.title, design: .rounded))
+                                .frame(width: 50, height: 50)
+                                .foregroundColor(.white)
+                                .background(Color(.blue))
+                                .clipShape(Circle())
+                                .padding(16)
+                                .shadow(color: Color(.black), radius: 6, x: 5, y: 5)
+                        }
+                    }
+                }
             }.navigationBarTitle("Inicio")
                 .navigationBarItems(leading: Button(action: {
                     self.loggout()
                 }, label: {
                     Text("Salir")
                 }))
+        }
+        .sheet(isPresented: self.$showModal) {
+            PostNewView(showModal: self.$showModal)
         }
     }
 }
---
+++ SwiftUI_11_Firebase/Views/PostNewView.swift
@@ -0,0 +1,39 @@
+//
+//  PostNewView.swift
+//  SwiftUI_11_Firebase
+//
+//  Created by Everis on 01/09/2020.
+//  Copyright © 2020 Alan. All rights reserved.
+//
+
+import SwiftUI
+
+struct PostNewView: View {
+    
+    @Binding var showModal: Bool
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack (alignment: .center, spacing: 40) {
+                    Button(action: {
+                        self.showModal = false
+                    }) {
+                        Text("Cancelar")
+                    }
+                    Button(action: {
+                        print("Publicar")
+                    }) {
+                        Text("Publicar")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct PostNewView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostNewView(showModal: .constant(false))
+    }
+}
